e16216da5000de5ba4b542ef30d3c6ec718f06dc
cklgaming <colinklassey33@gmail.com>
2024-11-23 22:03:31 -0500

    update node app

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ import fs from 'fs';
 import url from 'url';
 import path from 'path';
 
+// setup port, define file name and directory name variables
 const PORT = process.env.PORT;
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// 
 let pages = [];
 fs.readdirSync("./public/").forEach(file => {
                 pages.push(file);
 });
+console.log(pages);
 
+let writing = [];
+fs.readdirSync("./public/writing/").forEach(file => {
+    writing.push(file);
+});
+console.log(writing);
+
+// define url and path arrays
+// urls holds the file the way it should show up in the url
+// paths holds the file the way it is named in the fs
 let urls = [];
 let paths = [];
 for (let page in pages) {
@@ -20,12 +32,15 @@ for (let page in pages) {
         urls.push('/' + path.parse(pages[page]).name);
     }
 }
+console.log(urls);
+console.log(paths);
 
 const server = http.createServer(async(req, res) => {
     console.log(req.url, req.method);
     try {
         if (req.method === 'GET') {
             let filePath;
+            // figure out how to write 'for' loop in order to iterate through all pages
             for (_ in urls) {
                 if (req.url === '/') {
                     filePath = path.join(__dirname, 'public', 'index.html');
@@ -33,6 +48,11 @@ const server = http.createServer(async(req, res) => {
                 }
                 else if (req.url === urls[_]) {
                     filePath = path.join(__dirname, 'public', paths[_]);
+                    console.log(filePath);
+                }
+                // put writing pages in the same array as regular paths?
+                else if (req.url === writing[_]) {
+                    filePath = path.join(__dirname, 'public/writing', paths[_]);
                     console.log(filePath);
                 }
             }            
